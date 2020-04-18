9166bc4791385ac467338450f41e7874cf97c75c
chomeko3o <kusyopi@gmail.com>
2020-04-18 20:36:51 +0900

    studyモーダルをbody直下に、jsでbodyにoverflow:hiddenの追加 #11

--- src/js/study_modal.js
+++ src/js/study_modal.js
@@ -6,10 +6,12 @@ $(function () {
     winScrollTop = $(window).scrollTop();
     //モーダル出現
     $('.study__modal').fadeIn();
+    $('body').toggleClass('body__noscroll');
   });
   $('.study__modal__close').click(function () {
     //モーダル消す
     $('.study__modal').fadeOut();
+    $('body').toggleClass('body__noscroll');
     //モーダルをクリックした位置に戻す
     $('body,html').stop().animate({ scrollTop: winScrollTop }, 100);
   });
