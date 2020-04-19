fa11134e3d6561597e9feeaad8d8b6bbca25f2c2
chomeko3o <kusyopi@gmail.com>
2020-04-19 20:52:14 +0900

    モーダルの戻る方法を追加 #2

--- src/js/study_modal.js
+++ src/js/study_modal.js
@@ -6,13 +6,23 @@ $(function () {
     winScrollTop = $(window).scrollTop();
     //モーダル出現
     $('.study__modal').fadeIn();
+    //bodyをスクロールさせないようにする
+    //sassにてこのクラスを作り、overflow:hiddenを当てる
     $('body').toggleClass('body__noscroll');
   });
-  $('.study__modal__close').click(function () {
+  //2箇所のボタンをクリックでモーダルを閉じる。
+  //セレクタで２箇所指定できる
+  $('.study__modal__close,.study__modal__close2').click(function () {
     //モーダル消す
     $('.study__modal').fadeOut();
     $('body').toggleClass('body__noscroll');
     //モーダルをクリックした位置に戻す
     $('body,html').stop().animate({ scrollTop: winScrollTop }, 100);
+  });
+
+  //モーダルの背景をクリックしても、モーダルを閉じるようにする
+  $('.study__modal__bg').click(function () {
+    $('.study__modal').fadeOut();
+    $('body').toggleClass('body__noscroll');
   });
 });
